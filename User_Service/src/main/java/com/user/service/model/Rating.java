48bb9a1407133e2e8d0ec1e678a188bc8de361be
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2024-02-21 16:37:17 +0530

    add rest template

--- User_Service/src/main/java/com/user/service/model/Rating.java
+++ User_Service/src/main/java/com/user/service/model/Rating.java
@@ -1,5 +1,55 @@
 package com.user.service.model;
 
-public class Rating {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+
+public class Rating {
+	
+	private int ratingId;
+	private String userId;
+	private int hotelId;
+	private int rating;
+	private String feedBack;
+	private Hotel hotel;
+	public int getRatingId() {
+		return ratingId;
+	}
+	public void setRatingId(int ratingId) {
+		this.ratingId = ratingId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getHotelId() {
+		return hotelId;
+	}
+	public void setHotelId(int hotelId) {
+		this.hotelId = hotelId;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getFeedBack() {
+		return feedBack;
+	}
+	public void setFeedBack(String feedBack) {
+		this.feedBack = feedBack;
+	}
+	public Hotel getHotel() {
+		return hotel;
+	}
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+	
+	
+	
 }
--- User_Service/src/main/java/com/user/service/servicesimpl/UserServiceImpl.java
+++ User_Service/src/main/java/com/user/service/servicesimpl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.user.service.servicesimpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.user.service.model.Hotel;
+import com.user.service.model.Rating;
 import com.user.service.model.User;
 import com.user.service.repositry.UserServiceRepositry;
 import com.user.service.services.UserService;
@@ -12,6 +20,9 @@ import com.user.service.services.UserService;
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private RestTemplate restTemp;
+	
 	@Autowired
 	public UserServiceRepositry userServiceRepositry;
 	
@@ -22,12 +33,30 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<User> getAllUsers() {
-		return userServiceRepositry.findAll();
+		List<User> list = userServiceRepositry.findAll();
+		
+		list.forEach(s -> {
+			ArrayList<Rating> userList = restTemp.getForObject("http://Rating-Service/getRatingByUserId/"+s.getUserId(), ArrayList.class);
+			s.setRating(userList);
+		});
+		return list; 
+		
 	}
 
 	@Override
 	public User getUser(int id) {
-		return userServiceRepositry.findById(id).get();
+		User user =  userServiceRepositry.findById(id).get();
+		@SuppressWarnings("unchecked")
+		Rating[] RList = restTemp.getForObject("http://Rating-Service/getRatingByUserId/"+id, Rating[].class);
+		
+		List<Rating> list = Arrays.stream(RList).collect(Collectors.toList());
+		
+		list.forEach(p -> {
+			ResponseEntity<Hotel> hotel = restTemp.getForEntity("http://Hotel-Service/getHotel/"+p.getHotelId(), Hotel.class);
+			p.setHotel(hotel.getBody());
+		});
+		user.setRating(list);
+		return user;
 	}
 
 }
